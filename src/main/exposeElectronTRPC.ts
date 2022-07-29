06cbe3459ac0c43a1574cef2e7096d81f03ad974
Jason Nall <jsonnull@gmail.com>
2022-07-29 09:20:29 -0400

    Simplify API for exposing tRPC.

--- src/main/exposeElectronTRPC.ts
+++ src/main/exposeElectronTRPC.ts
@@ -1,8 +1,14 @@
-import type { IpcRenderer } from 'electron';
+import type { IpcRenderer, ContextBridge } from 'electron';
 import { TRPCHandlerArgs } from './types';
 
-export const exposeElectronTRPC = (ipcRenderer: IpcRenderer) => {
-  return {
+export const exposeElectronTRPC = ({
+  contextBridge,
+  ipcRenderer,
+}: {
+  contextBridge: ContextBridge;
+  ipcRenderer: IpcRenderer;
+}) => {
+  return contextBridge.exposeInMainWorld('electronTRPC', {
     rpc: (args: TRPCHandlerArgs) => ipcRenderer.invoke('electron-trpc', args),
-  };
+  });
 };
