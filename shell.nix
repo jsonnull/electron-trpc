e83210a1d4c9b3c40e585c205311060feff2fb11
Jason Nall <jsonnull@gmail.com>
2024-12-07 12:27:56 -0500

    Bump pnpm version.

--- shell.nix
+++ shell.nix
@@ -1,14 +1,27 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/8e65989a9972ce1da033f445d2598683590dfb8a.tar.gz") {} }:
+{ pkgs ? import (fetchTarball
+  "https://github.com/NixOS/nixpkgs/archive/8e65989a9972ce1da033f445d2598683590dfb8a.tar.gz")
+  { } }:
 
-pkgs.mkShell {
+let
+  nodejs = pkgs.nodejs_20;
+  electron = pkgs.electron_30;
+  pnpm = pkgs.nodePackages.pnpm.overrideAttrs (oldAttrs: rec {
+      version = "9.15.0";
+      src = pkgs.fetchurl {
+        url = "https://registry.npmjs.org/pnpm/-/pnpm-${version}.tgz";
+      sha512 = 
+          "sha512-duI3l2CkMo7EQVgVvNZije5yevN3mqpMkU45RBVsQpmSGon5djge4QfUHxLPpLZmgcqccY8GaPoIMe1MbYulbA==";
+      };
+  });
+in pkgs.mkShell {
   buildInputs = [
+    nodejs
+    electron
     pkgs.git
-    pkgs.nodejs_20
-    pkgs.nodePackages.pnpm
     pkgs.xvfb-run
-    pkgs.electron_30
+    pnpm
   ];
 
-  PLAYWRIGHT_ELECTRON_PATH="${pkgs.electron_30}/bin/electron";
-  PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=1;
+  PLAYWRIGHT_ELECTRON_PATH = "${electron}/bin/electron";
+  PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD = 1;
 }
